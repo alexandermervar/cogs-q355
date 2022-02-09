1319c544ccbdfbe44130438ce984a4323c05f39f
alexandermervar <alexandermervar@gmail.com>
2022-02-08 23:42:29 -0500

    Work as of 23:42 on 2.8.2022

--- Assignments/Assignment 4/Kohonen.py
+++ Assignments/Assignment 4/Kohonen.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 from math import dist
 
-fig, ax = plt.subplots(figsize=(1,1))
+fig, ax = plt.subplots(figsize=(10,10))
 
 # Define Learning Rate
 learningRate = 0.01
 
 # Define weight matrix size
 # TODO: Change to fit assignment
-xSize = 25
-ySize = 25
+xSize = 10
+ySize = 10
 weightVectorSize = 3
 
 # Define Frequency of Input Vectors
@@ -23,7 +23,7 @@ blueFrequency  = 0
 
 # Define the number of iterations
 # TODO: Change to fit assignment
-iterations = 100
+iterations = 1
 
 
 # Define an random X*Y*WV array
@@ -43,7 +43,6 @@ for i in range(iterations):
 
     # Traverse the matrix
     for j in range(xSize):
-        #
         for k in range(ySize):
             # Select the SOFM Unit
             selectedUnit = weightMatrix[j,k]
@@ -51,31 +50,53 @@ for i in range(iterations):
             # Calculate the external input (The dot product of the input vector and the weight vector)
             externalInput = np.dot(inputVector, selectedUnit)
 
+            # Calculate the lateral input for the selected unit
+            lateralInputSelectedUnit = externalInput * 8
+
+            # Calculate the eta of the selcted unit
+            if (lateralInputSelectedUnit + externalInput <= 0):
+                eta = 0
+            elif (lateralInputSelectedUnit + externalInput >= 5):
+                eta = 5
+            else:
+                eta = lateralInputSelectedUnit + externalInput
+
+
+            # Calculate the new weight vector for the selected unit
+            for l in range(weightVectorSize):
+                weightMatrix[j,k,l] = (weightMatrix[j,k,l] + learningRate * eta * (lateralInputSelectedUnit + externalInput))/dist(inputVector, selectedUnit)
+
             # Traverse to every other unit
             for l in range(xSize):
                 for m in range(ySize):
-                    # Select a new SOFM Unit
-                    selectedUnit = weightMatrix[l,m]
-
-                    # Calculate eta
-                    # TODO: Complete this
-                    eta = 
-
-                    # TODO: Add wrap-around mechanic
-                    # Calculate the lateral input
-                    if (dist(j,k,l,m) < 3):
-                        lateralInput = eta * 8
-                    elif (8 > dist(j,k,l,m) > 3):
-                        lateralInput = eta * -1
+                    if (i == l & j == m):
+                        continue
                     else:
-                        lateralInput = 0
+                        # Select a new SOFM Unit
+                        selectedUnit2 = weightMatrix[l,m]
+
+                        # TODO: Add wrap-around mechanic
+                        # Calculate the lateral input
+                        if (dist((j,k), (l,m)) < 3):
+                            lateralInput = eta * 8
+                        elif (8 > dist((j,k), (l,m)) > 3):
+                            lateralInput = eta * -1
+                        else:
+                            lateralInput = 0
+
+                        # Calculate the external input
+                        externalInput2 = np.dot(weightMatrix[j,k], selectedUnit2)
+
+                        # Calculate the change in weights
+                        for n in range(weightVectorSize):
+                            weightMatrix[l,m,n] = (weightMatrix[l,m,n] + learningRate * eta * (lateralInput + externalInput2))/dist(inputVector, selectedUnit2)
+
 # Produce an Image
 plt.imshow(weightMatrix)
 
 plt.show()
 
-# Commented out because I don't have "count" defined
-# ax.set_title('%f'%(count))
+ax.set_title("Mervar Kohonen Map")
 
 fig.canvas.draw()
 
