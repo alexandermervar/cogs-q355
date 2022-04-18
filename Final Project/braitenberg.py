80c14d75b9eb8d44e6b12f62dcb4d332a47f4716
alexandermervar <alexandermervar@gmail.com>
2022-04-18 17:30:55 -0400

    Started work on final project

---
+++ Final Project/braitenberg.py
@@ -0,0 +1,42 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Chaser:
+
+    def __init__(self):
+        # Here I initialize all of the variables for the Braitenberg class
+        self.xpos = 0.0                                       # Braitenberg vehicle's x position, starts in middle of world
+        self.ypos = 0.0                                       # Braitenberg vehicle's y position, starts in middle of world
+        self.orientation = np.random.random()*2*np.pi         # Braitenberg vehicle's orientation, starts at random
+        self.velocity = 0.0                                   # Braitenberg vehicle's velocity, starts at 0
+        self.radius = 1.0                                     # the size/radius of the Braitenberg vehicle
+        # The sensor variables are key for location of other vehicle
+        self.leftSensor = 0.0                                    # left sensor value
+        self.rightSensor = 0.0                                   # right sensor value
+        self.leftWheel  = 1.0                                  # left wheel output
+        self.rightWheel = 1.0                                  # right wheel output
+        
+        # Attributes to determine the placement of the sensors
+        self.angleoffset = np.pi/2                                                 # left/right sensor angle offset
+        self.rs_xpos = self.radius * np.cos(self.orientation + self.angleoffset)   # right sensor x position
+        self.rs_ypos = self.radius * np.sin(self.orientation + self.angleoffset)   # right sensor y position
+        self.ls_xpos = self.radius * np.cos(self.orientation - self.angleoffset)   # left sensor x position
+        self.ls_ypos = self.radius * np.sin(self.orientation - self.angleoffset)   # left sensor y position
+
+        # Chaser Attributes
+        self.caughtEvader = False                                                 # boolean to determine if the chaser has caught the evader
+
+    def readSensors(self, evader):
+        # This function is what is used to calibrate the sensors in order to track an Evader object
+
+        # Calculate the distance of the Evader for each of the sensors
+            self.leftSensor = 1 - np.sqrt((self.ls_xpos-evader.xpos)**2 + (self.ls_ypos-evader.ypos)**2)/10
+            self.leftSensor = np.clip(self.leftSensor,0,1)
+            self.rightSensor = 1 - np.sqrt((self.rs_xpos-evader.xpos)**2 + (self.rs_ypos-evader.ypos)**2)/10
+            self.rightSensor = np.clip(self.rightSensor,0,1)
+
+    def setWheels(self):
+        
+
+class Evader:
